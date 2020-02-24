639fe1274e24b25b74bffe635ea54491095d5bb4
framirezgarrido1 <f.ramirez@globant.com>
2020-02-24 17:28:02 -0300

    Cambio de respuesta MQTT

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ app.put('/api/update/:deviceId/:status', function(req, res) {
 		let now = new Date();
 
 		client.publish(`${device.topic}${device.device}`, "--------------------------------------------------------")
-		client.publish(`${device.topic}${device.device}`, `${device.device} > ${device.pin} > ${req.params.status}`)
+		client.publish(`${device.topic}${device.device}`, `${deviceId}-${req.params.status}`)
 		client.publish(`${device.topic}${device.device}`, `${deviceId} > Digital PIN ${device.pin} > status: ${req.params.status}`)
 		client.publish(`${device.topic}${device.device}`, `Fecha udpate > ${now}`)
 
