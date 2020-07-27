430880f1e5aa382b36b6306c05b22c3832012953
framirezgarrido1 <f.ramirez@globant.com>
2020-07-27 14:47:33 -0400

    Edit date Schema

--- models/devices.js
+++ models/devices.js
@@ -13,7 +13,7 @@ const DevicesSchema = Schema ({
 	timeon: String,
 	timeoff: String,
 	days: String,
-	creationDate: { type: Date, default: Date.now() }
+	creationDate: Date.now
 })
 
 module.exports = mongoose.model('Devices', DevicesSchema)
