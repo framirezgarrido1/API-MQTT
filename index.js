2d86a05f85de392137920e8a9830b6b3a9a05401
framirezgarrido1 <f.ramirez@globant.com>
2020-08-18 00:55:00 -0400

    PORT 3002

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ mongoose.connect("mongodb://localhost:27017/storage_devices", (err, res) => {
 	key: fs.readFileSync('key_2.pem'),
 	cert: fs.readFileSync('cert_2.pem')
   }, app)
-  .listen(2053, function () {
+  .listen(3002, function () {
 	console.log('Example app listening on port 3000! Go to https://localhost:3002/')
   })
 
